ebabbdd9a321315f40ba5b65e7765417b095048c
Arturo Huesca <tazkbron@gmail.com>
2016-01-16 00:10:06 +0000

    inicio

---
+++ programas/prog4.js
@@ -0,0 +1,16 @@
+var  suma= function (n1,n2)
+{
+result = n1 + n2;
+return result;
+}
+var resta = function (n1,n2)
+{
+  result = n1-n2;
+  return result;
+}
+suma1=suma(4,9);
+console.log(suma1);
+suma2=suma(2,' amigos')
+console.log(suma2)
+resta1=resta(4,9);
+console.log(resta1);
